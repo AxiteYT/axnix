40a8a5cc0097d356e5be3491b0412535c3b411ea
Axite <kylesmthh@gmail.com>
2024-03-18 18:21:10 +1100

    Revert

--- hosts/axnix.nix
+++ hosts/axnix.nix
@@ -20,9 +20,10 @@
       };
 
       # Enable the gnome desktop environment
-      displayManager.gdm.enable = true;
-      desktopManager.gnome.enable = true;
-
+      displayManager = {
+        gdm.enable = true;
+        gnome.enable = true;
+      };
     };
     /*
       # Enable the KDE Plasma Desktop environment
