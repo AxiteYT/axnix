d2217cc994502fb7f6fcfcaff9ad7fed69b75981
Axite <kylesmthh@gmail.com>
2024-03-18 17:52:40 +1100

    fixed bad line lol

--- hosts/axnix.nix
+++ hosts/axnix.nix
@@ -24,8 +24,8 @@
         gdm.enable = true;
         gnome.enable = true;
       };
-
-      /*
+    };
+    /*
       # Enable the KDE Plasma Desktop environment
       displayManager = {
         sddm = {
@@ -40,49 +40,49 @@
         desktopManager.plasma6.enable = true;
       */
 
-      # Enable CUPS to print documents.
-      printing.enable = true;
-    };
+    # Enable CUPS to print documents.
+    printing.enable = true;
+  };
 
-    # Theming
-    qt = {
+  # Theming
+  qt = {
+    enable = true;
+    style = "adwaita-dark";
+  };
+
+  programs.dconf.enable = true;
+
+  fonts = {
+    packages = with pkgs; [
+      font-awesome
+      hunspellDicts.en_AU
+      hunspellDicts.en_US
+      libreoffice-qt
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      source-han-sans
+      source-han-sans-japanese
+      source-han-serif-japanese
+      (nerdfonts.override { fonts = [ "Meslo" ]; })
+    ];
+    fontconfig = {
       enable = true;
-      style = "adwaita-dark";
-    };
-
-    programs.dconf.enable = true;
-
-    fonts = {
-      packages = with pkgs; [
-        font-awesome
-        hunspellDicts.en_AU
-        hunspellDicts.en_US
-        libreoffice-qt
-        noto-fonts
-        noto-fonts-cjk
-        noto-fonts-emoji
-        source-han-sans
-        source-han-sans-japanese
-        source-han-serif-japanese
-        (nerdfonts.override { fonts = [ "Meslo" ]; })
-      ];
-      fontconfig = {
-        enable = true;
-        defaultFonts = {
-          monospace = [ "Meslo LG M Regular Nerd Font Complete Mono" ];
-          serif = [ "Noto Serif" "Source Han Serif" ];
-          sansSerif = [ "Noto Sans" "Source Han Sans" ];
-        };
+      defaultFonts = {
+        monospace = [ "Meslo LG M Regular Nerd Font Complete Mono" ];
+        serif = [ "Noto Serif" "Source Han Serif" ];
+        sansSerif = [ "Noto Sans" "Source Han Sans" ];
       };
     };
+  };
 
-    # Define user account.
-    users.users.axite = {
-      isNormalUser = true;
-      description = "Kyle Smith";
-      extraGroups = [ "networkmanager" "wheel" "flatpak" ];
-    };
+  # Define user account.
+  users.users.axite = {
+    isNormalUser = true;
+    description = "Kyle Smith";
+    extraGroups = [ "networkmanager" "wheel" "flatpak" ];
+  };
 
-    # Enable wayland for Chromium/Electron based applications
-    environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  }
+  # Enable wayland for Chromium/Electron based applications
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+}
