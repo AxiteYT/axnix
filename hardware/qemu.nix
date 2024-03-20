827c2091eedaf7bf129d0f6ae8be648e98bfe9e3
Axite <kylesmthh@gmail.com>
2024-03-20 15:28:25 +1100

    qemu fix

--- hardware/qemu.nix
+++ hardware/qemu.nix
@@ -1,3 +1,11 @@
-{ config, lib, pkgs, modulesPath, ... }: {
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports = [
+    # Import the QEMU VM module for additional virtualisation options.
+    "${modulesPath}/virtualisation/qemu-vm.nix"
+  ];
+
+  # Configuration for the QEMU guest agent
   virtualisation.qemu.guestAgent.enable = true;
 }
