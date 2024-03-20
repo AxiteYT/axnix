497c00ea9dc99879483f761eaa15e01068bdc7d1
Axite <kylesmthh@gmail.com>
2024-03-20 15:40:30 +1100

    jeli bootloader

--- hosts/jeli/bootloader.nix
+++ hosts/jeli/bootloader.nix
@@ -1,11 +1,7 @@
-{ config, lib, pkgs, modulesPath, ... }:
-
 {
-  imports = [
-    # Import the QEMU VM module for additional virtualisation options.
-    "${modulesPath}/virtualisation/qemu-vm.nix"
-  ];
-
-  # Configuration for the QEMU guest agent
-  virtualisation.qemu.guestAgent.enable = true;
+  # Bootloader.
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+  };
 }
