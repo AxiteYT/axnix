a3b50379cb4bf99f7dadf58086efa1e64e99c015
Axite <kylesmthh@gmail.com>
2024-03-26 22:22:44 +1100

    Fixed disko /dev/*

--- hosts/besta/disko.nix
+++ hosts/besta/disko.nix
@@ -3,7 +3,7 @@
     disk = {
       main = {
         type = "disk";
-        device = "/dev/sda";
+        device = builtins.elemAt disks 0;
         content = {
           type = "gpt";
           partitions = {
