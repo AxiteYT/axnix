c02cd1189ed932ef0f3d1847c38a56a53ac31c00
Axite <kylesmthh@gmail.com>
2024-03-26 22:41:31 +1100

    Added besta hardware-config

--- hosts/besta/hardware-configuration.nix
+++ hosts/besta/hardware-configuration.nix
@@ -1,3 +1,25 @@
 {
-  
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+
+  boot = {
+    kernelModules = [
+      "kvm-intel"
+    ];
+    extraModulePackages = [
+    ];
+
+    initrd = {
+      avaliableKernelModules = [
+        "uhci_hcd"
+        "ehci_pci"
+        "ahci"
+        "virtio_pci"
+        "virtio_scsi"
+        "sd_mod"
+        "sr_mod"
+      ];
+      kernelModules = [
+      ];
+    };
+  };
 }
