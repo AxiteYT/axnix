72328e163c1e1c1fd057ec0d0a5a7ba942af50fe
Kyle Smith <kylesmthh@gmail.com>
2024-04-03 22:13:16 +1100

    Manual bootloader install implemented

--- scripts/install.sh
+++ scripts/install.sh
@@ -31,8 +31,6 @@ nix \
 # Install NixOS
 nixos-install --flake .#${desiredFlake} --root /mnt --no-root-password
 
-#TODO: Uncomment if bootloader doesn't install again.
-: <<'END'
 # Bootloader
 echo ######################################################################
 echo ""
@@ -43,7 +41,6 @@ echo ######################################################################
 for i in dev proc sys; do mount --rbind /$i /mnt/$i; done
 NIXOS_INSTALL_BOOTLOADER=1 chroot /mnt \
   /nix/var/nix/profiles/system/bin/switch-to-configuration boot
-END
 
 # Explain password setting
 echo ######################################################################
