6df295bd6f39ec6bca494bf77d78f6e18930dcc7
Kyle Smith <kylesmthh@gmail.com>
2024-04-03 21:22:04 +1100

    Fix for besta GRUB

--- hosts/besta/bootloader.nix
+++ hosts/besta/bootloader.nix
@@ -2,7 +2,9 @@
   # Bootloader.
   boot.loader.grub = {
     enable = true;
+    device = "nodev";
     efiSupport = true;
     efiInstallAsRemovable = true;
+    efiSysMountPoint = "/boot/efi";
   };
 }
